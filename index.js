c2ab3586398d509ae3fefcfe4fcf15cf4df6fe2b
IAmHim89 <computertech0418@gmail.com>
2024-06-04 19:00:36 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Create the `series` variable
 var series = [0, 1];
 
-while (/* your check here */) {
-  // your code here
-}
+while (series.length < 12) {
+   // your code here
+   
+   let call1 = series.pop();
+   let call2 = series.pop();
 
+   var sum = call2 + call1;
+    
+   
+   series.push(call2, call1, sum);
+   console.log(series);   
+  }  
 // Print the Fibonacci sequence to the console
-console.log(series);
 // the first ten numbers should be
 // 0 1 1 2 3 5 8 13 21 34
